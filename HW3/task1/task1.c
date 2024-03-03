001eab78093c7042c336525661ea1bdedb0c8560
ivan52945 <ivan52945@gmail.com>
2024-03-03 13:51:45 +0300

    feat:make task 1 HW3

---
+++ HW3/task1/task1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_input(int* pa, int* pb, int* pc) {
+    int n_items = 0;
+
+    n_items = scanf("%d%d%d", pa, pb, pc);
+    if(n_items != 3) {
+        printf("Error: invalid input, expected any 3 integer numbers\n");
+        abort();
+    }
+}
+
+
+int main() {
+    long sum;
+    int a, b, c;
+
+    read_input(&a, &b, &c);
+
+    sum = a + b + c;
+
+    printf("a + b + c = %ld", sum);
+}
