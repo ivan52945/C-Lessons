c9a3ce207897a8d149745a9bd4a9d938d0e012cc
ivan52945 <ivan52945@gmail.com>
2024-03-03 13:52:31 +0300

    feat:make task 3 HW3

---
+++ HW3/task3/task3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_input(int* pa, int* pb) {
+    int n_items = 0;
+
+    n_items = scanf("%d%d", pa, pb);
+    if(n_items != 2) {
+        printf("Error: invalid input, expected any 2 integer numbers\n");
+        abort();
+    }
+}
+
+
+int main() {
+    long dif;
+    int a, b;
+
+    read_input(&a, &b);
+
+    dif = (long)a - b;
+
+    printf("%d", dif);
+}
