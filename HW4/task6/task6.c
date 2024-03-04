64cfc95d8a78e9b2d1c9653e5f2b9bf88b27e222
ivan52945 <ivan52945@gmail.com>
2024-03-04 22:39:13 +0300

    feat: make task6 HW4

---
+++ HW4/task6/task6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_input(int* pa, int* pb) {
+    int n_items = 0;
+
+    n_items = scanf("%d%d", pa, pb);
+    if(n_items != 2) {
+        printf("Error: invalid input, expected any 2 integer numbers\n");
+        abort();
+    }
+}
+
+int main() {
+    int a, b;
+
+    read_input(&a, &b);
+
+    if(a > b) printf("Above\n");
+    else if(a == b) printf("Equal\n");
+    else printf("Less\n");
+
+    return 0;
+}
