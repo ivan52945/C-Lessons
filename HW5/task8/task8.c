ffd75f70e8a91731408d49452e499a18f4be812b
ivan52945 <ivan52945@gmail.com>
2024-03-08 17:00:43 +0300

    feat: make task8 HW5

---
+++ HW5/task8/task8.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_input(int* pa) {
+    int n_items = 0;
+
+    n_items = scanf("%d", pa);
+    if(n_items != 1) {
+        printf("Error: invalid input, expected any 1 integer\n");
+        abort();
+    }
+}
+
+int main() {
+    int a, digit;
+    int max = 0, min = 9;
+
+    read_input(&a);
+
+    if(a < 9) {
+        while(a > 0) {
+            digit = a % 10;
+
+            max = (digit > max) ? digit : max;
+            min = (digit < min) ? digit : min;
+
+            a /= 10;
+        }
+    }
+    else {
+        min = a;
+        max = a;
+    }
+
+    printf("%d %d\n", min, max);
+
+    return 0;
+}
