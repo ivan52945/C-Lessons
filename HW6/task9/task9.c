338cdf0229d302cea64df3496324961163921bf3
ivan52945 <ivan52945@gmail.com>
2024-03-13 17:32:52 +0300

    feat: make task9 HW6

---
+++ HW6/task9/task9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int count = 0;
+
+    char c;
+
+    while((c = getchar()) != '.') {
+        if(c == '(')
+            count += 1;
+        else if(c == ')')
+            count -= 1;
+    }
+
+    if(count == 0)
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
