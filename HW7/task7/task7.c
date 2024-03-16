2356af70883ffb24499bee1dd8958d73060bd238
ivan52945 <ivan52945@gmail.com>
2024-03-16 15:32:43 +0300

    feat: make task7 HW7

---
+++ HW7/task7/task7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int acounter(void) {
+    char a = getchar();
+
+    if(a == '.') return 0;
+
+    if(a == 'a')
+        return 1 + acounter();
+    else
+        return acounter();
+}
+
+int main() {
+
+    int acount = acounter();
+
+    printf("%d\n", acount);
+
+    return 0;
+}
